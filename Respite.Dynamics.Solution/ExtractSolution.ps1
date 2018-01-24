a8666e8ec26186cf82f1c09e684daacec7a27646
Luke Colburn <luke@sigaostudios.com>
2018-01-24 16:00:04 -0600

    test project

---
+++ Respite.Dynamics.Solution/ExtractSolution.ps1
@@ -0,0 +1,17 @@
+﻿param([string]$userName,
+[string]$password,
+[string]$crmUrl = "https://escrmt1.crm.dynamics.com")
+
+$gScriptPath = $MyInvocation.MyCommand.Path
+$rootFolder = (get-item $gScriptPath).Directory.parent.FullName
+$scriptPath = $rootFolder + "\lib\xrm-ci-framework\ExtractCustomizations.ps1"
+
+$solutionPackager = $rootFolder + "\lib\xrm-ci-framework\SolutionPackager.exe"
+$solutionPackager = "`"$solutionPackager`""
+$solutionFilesFolder = $rootFolder + "\Respite.Dynamics.Solution\extract"
+$solutionFilesFolder = "`"$solutionFilesFolder`""
+$solutionName = "RespiteCare"
+$connectionString = "`"AuthType=Office365;Username=$userName; Password=$password;Url=$crmUrl`""
+
+$argumentList = "-solutionPackager $solutionPackager -solutionFilesFolder $solutionFilesFolder -solutionName $solutionName -connectionString $connectionString -TreatPackWarningsAsErrors 0"
+Invoke-Expression "& `"$scriptPath`" $argumentList"
